bb3936338e2e77b79560996125a18364364fb566
Raphael Wouters <rawouter@cisco.com>
2016-07-29 23:39:43 +0200

    Implemented jira assign

--- jira.py
+++ jira.py
@@ -4,13 +4,16 @@ from errbot import botcmd
 from itertools import chain
 import re
 
-CONFIG_TEMPLATE = {'API_URL': 'http://jira.example.com',
-                   'USERNAME': 'errbot',
-                   'PASSWORD': 'password',
-                   'OAUTH_ACCESS_TOKEN': None,
-                   'OAUTH_ACCESS_TOKEN_SECRET': None,
-                   'OAUTH_CONSUMER_KEY': None,
-                   'OAUTH_KEY_CERT_FILE': None}
+CONFIG_TEMPLATE = {
+    'API_URL': 'http://jira.example.com',
+    'USERNAME': 'errbot',
+    'PASSWORD': 'password',
+    'PROJECTS': ['FOO', 'BAR'],
+    'OAUTH_ACCESS_TOKEN': None,
+    'OAUTH_ACCESS_TOKEN_SECRET': None,
+    'OAUTH_CONSUMER_KEY': None,
+    'OAUTH_KEY_CERT_FILE': None
+}
 
 try:
     from jira import JIRA, JIRAError
@@ -28,7 +31,6 @@ class Jira(BotPlugin):
         Login to Jira with OAUTH
         """
         api_url = self.config['API_URL']
-        # TODO(alex) make this check more robust
         if self.config['OAUTH_ACCESS_TOKEN'] is None:
             message = 'oauth configuration not set'
             self.log.info(message)
@@ -122,6 +124,28 @@ class Jira(BotPlugin):
         """
         return CONFIG_TEMPLATE
 
+    def _send_msg(self, msg, message):
+        self.send(msg.frm,
+                  message,
+                  message_type=msg.type,
+                  in_reply_to=msg,
+                  groupchat_nick_reply=True)
+
+    def _find_one_user(self, msg, userstring):
+        """
+        Return one jira user, if zero or more than one user found, return None and send a message.
+        """
+        users = self.jira_connect.search_assignable_users_for_projects(userstring, self.config['PROJECTS'])
+        if len(users) == 0:
+            self._send_msg(msg, 'No corresponding user found: {}'.format(userstring))
+            user = None
+        elif len(users) > 1:
+            self._send_msg(msg, 'Too many user found: {}'.format(', '.join([u.name for u in users])))
+            user = None
+        else:
+            user = users[0]
+        return user
+
     def _verify_issue_id(self, msg, issue):
         if issue == '':
             self.send(msg.frm,
@@ -145,7 +169,7 @@ class Jira(BotPlugin):
         Returns the subject of the issue and a link to it.
         """
         issue = self._verify_issue_id(msg, args.pop(0))
-        if issue is '':
+        if issue is None:
             return
         jira = self.jira_connect
         try:
@@ -163,11 +187,7 @@ class Jira(BotPlugin):
                 in_reply_to=msg
             )
         except JIRAError:
-            self.send(msg.frm,
-                      'Issue {} not found!'.format(issue),
-                      message_type=msg.type,
-                      in_reply_to=msg,
-                      groupchat_nick_reply=True)
+            self._send_msg(msg, 'Issue {} not found!'.format(issue))
 
     @botcmd(split_args_with=' ')
     def jira_create(self, msg, args):
@@ -177,14 +197,26 @@ class Jira(BotPlugin):
         """
         return "Not implemented"
 
-    @botcmd(split_args_with=' ')
+    @botcmd(split_args_with=None)
     def jira_assign(self, msg, args):
         """
         (Re)assigns an issue to a given user
-        not implemented yet
+        Usage: jira assign <issue_id> <username>
         """
-        return "Not implemented"
-
+        if len(args) != 2:
+            self._send_msg(msg, 'Usage: jira assign <issue_id> <username>')
+            return
+        jira = self.jira_connect
+        issueid = self._verify_issue_id(msg, args[0])
+        if issueid is None:
+            return
+        user = self._find_one_user(msg, args[1])
+        try:
+            issue = jira.issue(issueid)
+            jira.assign_issue(issue, user.name)
+            self._send_msg(msg, 'Issue {} assigned to {}'.format(issue, user))
+        except JIRAError:
+            self._send_msg(msg, 'Issue {} not found!'.format(issue))
 
 def verify_and_generate_issueid(issueid):
     """
@@ -193,10 +225,10 @@ def verify_and_generate_issueid(issueid):
     """
     matches = []
     regexes = []
-    regexes.append(r'([^\w\d_]+)\-(\d+)')  # e.g.: issue-1234
-    regexes.append(r'([^\w\d_]+)(\d+)')    # e.g.: issue1234
+    regexes.append(r'([^\W\d_]+)\-(\d+)')  # e.g.: issue-1234
+    regexes.append(r'([^\W\d_]+)(\d+)')    # e.g.: issue1234
     for regex in regexes:
-        matches.extend(re.findall(regex, issueid, flags=re.i | re.u))
+        matches.extend(re.findall(regex, issueid, flags=re.I | re.U))
     if matches:
         for match in set(matches):
             return match[0].upper() + '-' + match[1]
